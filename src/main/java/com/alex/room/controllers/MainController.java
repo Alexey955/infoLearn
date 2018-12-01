ab44cd66f609c2b031885f6cb8abebb58358e0f6
Aleksey <alekseyvasiljev@gmail.com>
2018-12-01 21:47:46 +0300

    With ThymLeaf 1.0

--- src/main/java/com/alex/room/controllers/MainController.java
+++ src/main/java/com/alex/room/controllers/MainController.java
@@ -1,18 +1,26 @@
 package com.alex.room.controllers;
 
 import com.alex.room.repos.TableInfoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
 @Controller
 public class MainController {
-
+    @Autowired
     private TableInfoRepo tableInfoRepo;
 
     @GetMapping("/")
     public String startPage(Map<String, Object> model) {
-        return "test";
+        return "mainPage.html";
+    }
+
+    @PostMapping("/addNewElem")
+    public String addElem() {
+        return "addNewElem";
     }
 }
